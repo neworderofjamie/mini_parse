4d861f02f0a80fb690b9aba5569300d20a9c9960
neworderofjamie <neworderofjamie@gmail.com>
2021-12-02 12:55:03 +0000

    first commit - semi-functional scanner!

---
+++ src/main.cc
@@ -0,0 +1,91 @@
+// Standard C++ includes
+#include <iostream>
+#include <regex>
+#include <string>
+
+#include "scanner.h"
+
+std::string test(
+    "if($(outRow) == $(maxOutRow)) {\n"
+    "   $(endRow);\n"
+    "}\n"
+    "const int strideRow = ($(outRow) * (int)$(conv_sh)) - (int)$(conv_padh);\n"
+    "const int kernRow = $(inRow) - strideRow;\n"
+    "for(int outCol = $(minOutCol); outCol < $(maxOutCol); outCol++) {\n"
+    "    const int strideCol = (outCol * (int)$(conv_sw)) - (int)$(conv_padw);\n"
+    "    const int kernCol = $(inCol) - strideCol;\n"
+    "    for(unsigned int outChan = 0; outChan < (unsigned int)$(conv_oc); outChan++) {\n"
+    "        const int idPost = (($(outRow) * (int)$(conv_ow) * (int)$(conv_oc)) +\n"
+    "                            (outCol * (int)$(conv_oc)) +\n"
+    "                            outChan);\n"
+    "        $(addSynapse, idPost, kernRow, kernCol, $(inChan), outChan);\n"
+    "    }\n"
+    "}\n"
+    "$(outRow)++;\n");
+
+std::string test2(
+    "if ($(RefracTime) <= 0.0) {\n"
+    "  scalar alpha = (($(Isyn) + $(Ioffset)) * $(Rmembrane)) + $(Vrest);\n"
+    "  $(V) = alpha - ($(ExpTC) * (alpha - $(V)));\n"
+    "}\n"
+    "else {\n"
+    "  $(RefracTime) -= DT;\n"
+    "}\n");
+
+std::string test3(
+    "scalar Imem;\n"
+    "unsigned int mt;\n"
+    "scalar mdt= DT/25.0;\n"
+    "for (mt=0; mt < 25; mt++) {\n"
+    "   Imem= -($(m)*$(m)*$(m)*$(h)*$(gNa)*($(V)-($(ENa)))+\n"
+    "       $(n)*$(n)*$(n)*$(n)*$(gK)*($(V)-($(EK)))+\n"
+    "       $(gl)*($(V)-($(El)))-$(Isyn));\n"
+    "   scalar _a;\n"
+    "   if (lV == -52.0) {\n"
+    "       _a= 1.28;\n"
+    "   }\n"
+    "   else {\n"
+    "       _a= 0.32*(-52.0-$(V))/(exp((-52.0-$(V))/4.0)-1.0);\n"
+    "   }\n"
+    "   scalar _b;\n"
+    "   if (lV == -25.0) {\n"
+    "       _b= 1.4;\n"
+    "   }\n"
+    "   else {\n"
+    "       _b= 0.28*($(V)+25.0)/(exp(($(V)+25.0)/5.0)-1.0);\n"
+    "   }\n"
+    "   $(m)+= (_a*(1.0-$(m))-_b*$(m))*mdt;\n"
+    "   _a= 0.128*exp((-48.0-$(V))/18.0);\n"
+    "   _b= 4.0 / (exp((-25.0-$(V))/5.0)+1.0);\n"
+    "   $(h)+= (_a*(1.0-$(h))-_b*$(h))*mdt;\n"
+    "   if (lV == -50.0) {\n"
+    "       _a= 0.16;\n"
+    "   }\n"
+    "   else {\n"
+    "       _a= 0.032*(-50.0-$(V))/(exp((-50.0-$(V))/5.0)-1.0);\n"
+    "   }\n"
+    "   _b= 0.5*exp((-55.0-$(V))/40.0);\n"
+    "   $(n)+= (_a*(1.0-$(n))-_b*$(n))*mdt;\n"
+    "   $(V)+= Imem/$(C)*mdt;\n"
+    "}\n");
+
+std::string removeOldStyleVar(const std::string &input) {
+    std::regex variable(R"(\$\(([_a-zA-Z][a-zA-Z0-9]*)\))");
+ 
+    return std::regex_replace(input, variable, "$1");
+}
+int main()
+{
+    const auto test2Fix = removeOldStyleVar(test2);
+    try
+    {
+        const auto tokens = Parser::scanTokens(test2Fix);
+    }
+    catch(const Parser::ScannerError &s) {
+        std::cerr << s.what() << std::endl;
+    }
+    catch(const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+    return EXIT_SUCCESS;
+}
---
+++ src/scanner.cc
@@ -0,0 +1,311 @@
+#include "scanner.h"
+
+// Standard C++ includes
+#include <charconv>
+#include <unordered_map>
+
+// Standard C includes
+#include <cctype>
+
+using namespace Parser;
+
+// ---------------------------------------------------------------------------
+// Anonymous namespace
+// ---------------------------------------------------------------------------
+namespace
+{
+const std::unordered_map<std::string_view, Token::Type> keywords{
+    {"const", Token::Type::CONST},
+    {"do", Token::Type::DO},
+    {"else", Token::Type::ELSE},
+    {"false", Token::Type::FALSE},
+    {"for", Token::Type::FOR},
+    {"if", Token::Type::IF},
+    {"true", Token::Type::TRUE},
+    {"while", Token::Type::WHILE}};
+
+bool isodigit(char c)
+{
+    return (c >= '0' && c <= '7');
+}
+
+template<typename T>
+T toCharsThrow(std::string_view input, std::chars_format format = std::chars_format::general)
+{
+    T out;
+    const auto result = std::from_chars(input.data(), input.data() + input.size(), out);
+    if(result.ec == std::errc::invalid_argument) {
+        throw std::invalid_argument("Unable to convert chars '" + std::string{input} + "'");
+    }
+    else if(result.ec == std::errc::result_out_of_range) {
+        throw std::out_of_range("Unable to convert chars '" + std::string{input} + "'");
+    }
+    return out;
+}
+
+void scanIntegerSuffix(Cursor &cursor)
+{
+    // Read suffix
+    // **TODO** complete
+    while(std::tolower(cursor.peek()) == 'u' || std::tolower(cursor.peek()) == 'l') {
+        cursor.advance();
+    }
+}
+
+void scanNumber(Cursor &cursor, std::vector<Parser::Token> &tokens) 
+{
+    // If this is a hexadecimal literal
+    if(cursor.peek() == '0' && std::tolower(cursor.peekNext()) == 'x') {
+        // Advance past
+        cursor.advance();
+        cursor.advance();
+
+        // Read hexadecimal digits
+        while(std::isxdigit(cursor.peek())) {
+            cursor.advance();
+        }
+
+        // Read decimal place
+        bool isFloat = false;
+        if(cursor.peek() == '.') {
+            isFloat = true;
+            cursor.advance();
+        }
+
+        // Read hexadecimal digits
+        while(std::isxdigit(cursor.peek())) {
+            cursor.advance();
+        }
+
+        // If number is float
+        if(isFloat) {
+            // Check there's an exponent as these are REQUIRED for floating point literals
+            if(cursor.peek() != 'p') {
+                throw ScannerError(cursor.getLine(), "Hexadecimal floating point literal missing exponent.");
+            }
+            else {
+                // Read p
+                cursor.advance();
+
+                // Read sign
+                if(cursor.peek() == '-' || cursor.peek() == '+') {
+                    cursor.advance();
+                }
+
+                // Read DECIMAL digits
+                while(std::isdigit(cursor.peek())) {
+                    cursor.advance();
+                }
+
+                // If literal has floating point suffix
+                const char suffix = std::tolower(cursor.peek());
+                if(suffix == 'f') {
+                    // Add single-precision token
+                    // **NOTE** skip 0x prefix
+                    tokens.emplace_back(Token::Type::NUMBER, cursor,
+                                        toCharsThrow<float>(cursor.getLexeme().substr(2), std::chars_format::hex));
+
+                    // Advance
+                    // **NOTE** we do this AFTER parsing float as std::to_chars doesn't deal with suffixes
+                    cursor.advance();
+                }
+                // Add double-precision token
+                // **NOTE** skip 0x prefix
+                else {
+                    tokens.emplace_back(Token::Type::NUMBER, cursor,
+                                        toCharsThrow<double>(cursor.getLexeme().substr(2), std::chars_format::hex));
+                }
+            }
+        }
+        // Otherwise, number is hexadecimal integer
+        else {
+            scanIntegerSuffix(cursor);
+
+            // Add integer token
+            // **TODO** different types
+            // **NOTE** skip 0x prefix
+            tokens.emplace_back(Token::Type::NUMBER, cursor,
+                                toCharsThrow<int64_t>(cursor.getLexeme().substr(2), std::chars_format::hex));
+        }
+    }
+    // Otherwise, if this is an octal integer
+    else if(cursor.peek() == '0' && isodigit(cursor.peekNext())){
+        throw ScannerErrorUnsupported(cursor.getLine(), "Octal literals unsupported.");
+    }
+    // Otherwise, if it's decimal
+    else {
+        // Read digits
+        while(std::isdigit(cursor.peek())) {
+            cursor.advance();
+        }
+
+        // Read decimal place
+        bool isFloat = false;
+        if(cursor.peek() == '.') {
+            isFloat = true;
+            cursor.advance();
+        }
+
+        // Read digits
+        while(std::isdigit(cursor.peek())) {
+            cursor.advance();
+        }
+
+        // If it's float
+        if(isFloat) {
+            // If there's an exponent
+            if(cursor.peek() != 'e') {
+                // Read e
+                cursor.advance();
+
+                // Read sign
+                if(cursor.peek() == '-' || cursor.peek() == '+') {
+                    cursor.advance();
+                }
+
+                // Read digits
+                while(std::isdigit(cursor.peek())) {
+                    cursor.advance();
+                }
+
+                // If literal has floating point suffix
+                const char suffix = std::tolower(cursor.peek());
+                if(suffix == 'f') {
+                    // Add single-precision token
+                    tokens.emplace_back(Token::Type::NUMBER, cursor,
+                                        toCharsThrow<float>(cursor.getLexeme()));
+
+                    // Advance
+                    // **NOTE** we do this AFTER parsing float as std::to_chars doesn't deal with suffixes
+                    cursor.advance();
+                }
+                // Otherwise, add double-precision token
+                else {
+                    tokens.emplace_back(Token::Type::NUMBER, cursor,
+                                        toCharsThrow<double>(cursor.getLexeme()));
+                }
+            }
+        }
+        // Otherwise, number is integer
+        else {
+            scanIntegerSuffix(cursor);
+
+            // Add integer token
+            // **TODO** different types
+            tokens.emplace_back(Token::Type::NUMBER, cursor,
+                                toCharsThrow<int64_t>(cursor.getLexeme()));
+        }
+    }
+}
+
+void scanIdentifier(Cursor &cursor, std::vector<Parser::Token> &tokens)
+{
+    // Read subsequent alphanumeric characters and underscores
+    while(std::isalnum(cursor.peek()) || cursor.peek() == '_') {
+        cursor.advance();
+    }
+
+    // If identifier is a keyword, add appropriate token
+    const auto k = keywords.find(cursor.getLexeme());
+    if(k != keywords.cend()) {
+        tokens.emplace_back(k->second, cursor);
+    }
+    // Otherwise, add identifier token
+    // **TODO** EXT_IDENTIFIER
+    else {
+        tokens.emplace_back(Token::Type::INT_IDENTIFIER, cursor);
+    }
+}
+
+void scanToken(Cursor &cursor, std::vector<Parser::Token> &tokens)
+{
+    using namespace Parser;
+
+    char c = cursor.advance();
+    switch(c) {
+        // Single character tokens
+        case '(': tokens.emplace_back(Token::Type::LEFT_PAREN, cursor); break;
+        case ')': tokens.emplace_back(Token::Type::RIGHT_PAREN, cursor); break;
+        case '{': tokens.emplace_back(Token::Type::LEFT_BRACE, cursor); break;
+        case '}': tokens.emplace_back(Token::Type::RIGHT_BRACE, cursor); break;
+        case ',': tokens.emplace_back(Token::Type::COMMA, cursor); break;
+        case '.': tokens.emplace_back(Token::Type::DOT, cursor); break;
+        case '-': tokens.emplace_back(Token::Type::MINUS, cursor); break;
+        case '+': tokens.emplace_back(Token::Type::PLUS, cursor); break;
+        case ';': tokens.emplace_back(Token::Type::SEMICOLON, cursor); break;
+        case '*': tokens.emplace_back(Token::Type::STAR, cursor); break;
+
+        // Operators
+        case '!': tokens.emplace_back(cursor.match('=') ? Token::Type::NOT_EQUAL : Token::Type::NOT, cursor); break;
+        case '=': tokens.emplace_back(cursor.match('=') ? Token::Type::EQUAL_EQUAL : Token::Type::EQUAL, cursor); break;
+        case '<': tokens.emplace_back(cursor.match('=') ? Token::Type::LESS_EQUAL : Token::Type::LESS, cursor); break;
+        case '>': tokens.emplace_back(cursor.match('=') ? Token::Type::GREATER_EQUAL : Token::Type::GREATER, cursor); break;
+
+        case '/':
+        {
+            // Line comment
+            if(cursor.match('/')) {
+                while(cursor.peek() != '\n' && !cursor.isAtEnd()) {
+                    cursor.advance();
+                }
+            }
+            else {
+                tokens.emplace_back(Token::Type::SLASH, cursor);
+            }
+            break;
+        }
+
+        // Whitespace
+        case ' ':
+        case '\r':
+        case '\t':
+            break;
+
+        // New line
+        case '\n': cursor.nextLine(); break;
+
+        default:
+        {
+            // If we have a digit or a period, scan number
+            if(std::isdigit(c) || c == '.') {
+                scanNumber(cursor, tokens);
+            }
+            // Otherwise, if 
+            else if(c == '$') {
+
+            }
+            // Otherwise, scan identifier
+            else if(std::isalpha(c) || c == '_') {
+                scanIdentifier(cursor, tokens);
+            }
+            else {
+                throw ScannerError(cursor.getLine(), "Unexpected character.");
+            }
+        }
+    }
+}
+}
+// ---------------------------------------------------------------------------
+// Parser
+// ---------------------------------------------------------------------------
+namespace Parser
+{
+std::vector<Token> scanTokens(const std::string_view &source)
+{
+    std::vector<Token> tokens;
+
+    Cursor cursor(source);
+
+    // Current line
+    size_t line = 1;
+
+    // Scan tokens
+    while(!cursor.isAtEnd()) {
+        cursor.resetLexeme();
+        scanToken(cursor, tokens);
+    }
+
+    tokens.emplace_back(Token::Type::END_OF_FILE, cursor);
+    return tokens;
+}
+}
